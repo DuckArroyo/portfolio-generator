0da26f665012ac1fcf9b0122568ea901a4ed5f0e
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-26 22:13:48 -0700

    feature completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 // console.log(profileDataArgs);
 const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
 //! This two variables are replaced by line 7(third line)
 //This feature is called assignment destructuring
@@ -8,41 +9,8 @@ const fs = require("fs");
 // const github = profileDataArgs[1];
 const [name, github] = profileDataArgs;
 
-// const printProfileData = (profileDataArr) => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log("================");
-
-//   // Is the same as this...
-//   profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta chartset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-  <h1>${name}</h1>
-  <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-`;
-};
-
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
 
   console.log("Portfolio complete! Checkout index.html to see the output!");
 });
