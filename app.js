947dcb4bd3aebbf94a2ae1edd9048900ae6d41a1
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-27 23:35:30 -0700

    completed 9.4.4, code working

--- app.js
+++ app.js
@@ -1,52 +1,61 @@
-// const fs = require("fs");
+const fs = require("fs");
 const inquirer = require("inquirer");
-// const generatePage = require("./src/page-template.js");
+const generatePage = require("./src/page-template.js");
 
-//!Continue on module 9.3.7 - Use inquirer
+//!Continue on module 9.4.5 -
 
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name? (Required)",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter your name!");
-          return false;
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your GitHub Username: (Required)",
-      validate: (githubInput) => {
-        if (githubInput) {
-          return true;
-        } else {
-          console.log("Please enter your GitHub username");
-          return false;
-        }
-      },
-    },
-    {
-      type: "confirm",
-      name: "confirmAbout",
-      message:
-        'Would you like to enter some information about yourself for an "About" section?',
-      default: true,
-    },
-    {
-      type: "input",
-      name: "about",
-      message: "Provide some information about yourself:",
-      when: ({ confirmAbout }) => confirmAbout,
-    },
-  ]);
+//TODO COMMENTED IN FOR DEVELOPMENT
+const mockData = {
+  name: "Lernantino",
+  github: "lernantino",
+  projects: [],
 };
+
+//!Commented out for development - reinstate once development is completed.
+// const promptUser = () => {
+//   return inquirer.prompt([
+//     {
+//       type: "input",
+//       name: "name",
+//       message: "What is your name? (Required)",
+//       validate: (nameInput) => {
+//         if (nameInput) {
+//           return true;
+//         } else {
+//           console.log("Please enter your name!");
+//           return false;
+//         }
+//       },
+//     },
+//     {
+//       type: "input",
+//       name: "github",
+//       message: "Enter your GitHub Username: (Required)",
+//       validate: (githubInput) => {
+//         if (githubInput) {
+//           return true;
+//         } else {
+//           console.log("Please enter your GitHub username");
+//           return false;
+//         }
+//       },
+//     },
+//     {
+//       type: "confirm",
+//       name: "confirmAbout",
+//       message:
+//         'Would you like to enter some information about yourself for an "About" section?',
+//       default: true,
+//     },
+//     {
+//       type: "input",
+//       name: "about",
+//       message: "Provide some information about yourself:",
+//       when: ({ confirmAbout }) => confirmAbout,
+//     },
+//   ]);
+// };
+//!Reinstate ends
 
 const promptProject = (portfolioData) => {
   console.log(`
@@ -128,13 +137,19 @@ const promptProject = (portfolioData) => {
     });
 };
 
-promptUser()
-  .then(promptProject)
-  .then((portfolioData) => {
-    console.log(portfolioData); //! leave this console log for later. if portfolioData is surrounded by [], it will spit out objects.
-    // const pageHTML = generatePage(name, github);
-    // fs.writeFile("./index.html", pageHTML, (err) => {
-    //   if (err) throw err;
-    //   console.log("Portfolio complete! Check out index.html to see the output!");
-    // });
-  });
+//TODO - MOCK Data
+const pageHTML = generatePage(mockData);
+
+//!reinstate after development
+// promptUser()
+//   .then(promptProject)
+//   .then((portfolioData) => {
+//console.log(portfolioData); //! leave this console log for later. if portfolioData is surrounded by [], it will spit out objects.
+//const pageHTML = generatePage(portfolioData);
+fs.writeFile("./index.html", pageHTML, (err) => {
+  if (err) throw new Err(err);
+  console.log(
+    "Page created! Check out index.html in this directory to see it!"
+  );
+});
+// });
